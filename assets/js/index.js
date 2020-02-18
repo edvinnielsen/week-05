b88cbb2721eadd6d68f84b0a7d833d24dd99243d
Edvin Nielsen Johansson <enielsenjohansson@hawk.iit.edu>
2020-02-18 11:16:32 -0600

    Examples of JS types.

---
+++ assets/js/index.js
@@ -0,0 +1,110 @@
+// console.log("Hello World!");
+// console.debug("1 + 4");
+// console.error(1 + 4);
+// x=4*4
+// console.log("4 * 4 is equal to:", x)
+
+// This is an example of using "var", don't.
+// var x = 3;
+// var y = x + 2;
+// console.log(y)
+// y = "Hello"
+// console.log(y)
+
+//This is an example of using "let"
+// let x = 3;
+// let y = x + 2;
+// console.log(y);
+// y = "Hello";
+// console.log(y);
+
+// This is an example of using "const"
+// const today = "2020-02-18"
+// console.log(today)
+// today = "2020-02-19"
+// console.log(today)
+
+//Types
+
+    // //number
+    // let apples = 3;
+    // console.log(apples)
+
+    // //string
+    // let name = "Edvin";
+    // console.log(name)
+    // let sentence = "He calls himself \"Edvin\"";
+    // console.log(sentence)
+
+    // //new string
+    // let fullName = `${name} Nielsen`;
+    // console.log(fullName)
+
+    // //boolean
+    // let isOver18 = true;
+    // console.log(isOver18);
+
+    // //undefined
+    // let awesome;
+    // let cool = undefined;
+    // console.log("Varable 'awesome' is:", awesome);
+    // console.log("Varable 'cool' is:", cool);
+
+    // //null
+    // let winners = null;
+    // console.log("Varable 'winners' is:", winners);
+
+    // //array
+    // let groceries =
+    // [
+    //     "apples",
+    //     "orange",
+    //     "celery",
+    //     "pinapple",
+    // ];
+    // console.log(groceries);
+    // console.log(groceries.length);
+    // console.log("List item #1 is:", groceries[0]);
+    // console.log("Pinapples is at list #" + (groceries.indexOf("pinapple") + 1));
+
+    // groceries.push("eggs");
+    // console.log(groceries);
+    // groceries.push("ham", "cheese");
+    // console.log(groceries);
+
+    // console.log(groceries.length);
+    // let justBought = groceries.pop();
+    // console.log(groceries.length);
+    // console.log(justBought);
+
+    // // Objects
+    // let person =
+    // {
+    //     name:
+    //     {
+    //         first: "Edvin",
+    //         middle: null,
+    //         last: "Nielsen",
+    //     },
+
+    //     dateOfBirth: "January 17",
+    //     hair: "brown",
+    //     hairStyle: "slightly curly",
+
+    //     favColors:
+    //     [
+    //         "black",
+    //         "green",
+    //         "red",
+    //     ]
+    // }
+    // console.log(person.name.first);
+    // console.log(person.favColors[1]);
+
+    // console.log(person.dateOfDeath);
+    // person.dateOfDeath = "2020-02-18";
+    // console.log(person.dateOfDeath);
+    // person.dateOfDeath = null;
+    // console.log(person.dateOfDeath);
+
+
